f0283b3dd1560b8bcf2fcf6a41cc441372c558d0
srb1998 <80816037+srb1998@users.noreply.github.com>
2021-07-01 20:35:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,11 +26,12 @@ app.post("/subscribe", function (req, res) {
     ],
   };
   const jsonData = JSON.stringify(data);
-
-  const url = "https://us6.api.mailchimp.com/3.0/lists/c07e9782c6";
+  
+// For security purpose my api key and unique key is hidden
+  const url = "hidden";
   const options = {
     method: "POST",
-    auth: "sourabh:0b432a8484576e1acb6424b184abc4b2-us6",
+    auth: "sourabh: hidden",
   };
 
   const request = https.request(url, options, function (response) {
@@ -51,6 +52,4 @@ app.listen(process.env.PORT || 3000, function () {
   console.log("Server is up and running at port 3000");
 });
 
-// api key - 0b432a8484576e1acb6424b184abc4b2-us60b432a8484576e1acb6424b184abc4b2-us6
 
-// unique id - c07e9782c6
